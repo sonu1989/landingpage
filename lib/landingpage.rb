0ffd9f502e32fc4ddb8c42ed156280b4ffe003d9
sonus.ror@gmail.com <Sonu Singh>
2022-11-23 16:50:09 +0530

    initial commit

---
+++ lib/landingpage.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "landingpage/version"
+
+module Landingpage
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/landingpage/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Landingpage
+  VERSION = "0.1.0"
+end
